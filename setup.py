cd04a2fe423671c46b16bb354695715014ce4402
Nina Pavlich <nina@ninalp.com>
2017-11-01 11:52:09 -0700

    tagging 0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-ace-overlay',
-    version='0.6',
+    version='0.7',
     author='Nina Pavlich',
     author_email='nina@ninalp.com',
     url='https://github.com/ninapavlich/django-ace-overlay',
