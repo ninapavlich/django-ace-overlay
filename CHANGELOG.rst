2f4e0bd3e9149f20bd40d71485ff5e23c6df8074
Nina Pavlich <nina@hello-upswell.com>
2018-08-28 15:12:27 -0700

    create new release for django 2.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,3 +1,5 @@
 Changelog
 =========
 
+## [0.8.0] - 2018-08-28
+- Added Django 2.0 compatibility
--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='django-ace-overlay',
-    version='0.7.1',
+    version='0.8.0',
     author='Nina Pavlich',
     author_email='nina@ninalp.com',
     url='https://github.com/ninapavlich/django-ace-overlay',
     license="MIT",
     description='Ace editor for Django admin',
-    keywords=['libraries', 'web development', 'cms', 'django', 'code-editor', 'admin'],
+    keywords=['libraries', 'web development',
+              'cms', 'django', 'code-editor', 'admin'],
     include_package_data=True,
     packages=['ace_overlay'],
     classifiers=[
